795e1d29718970e487ae570bd07b95eb64263378
Vignesh <vignesh24cs@rnsit.ac.in>
2026-05-05 23:19:43 +0530

    h

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.appendFile("hey.txt","I am Fine ",function(err){
-    if(err) console.error(err);
-    else console.log("done");
-})
+// // fs.appendFile("hey.txt","I am Fine ",function(err){
+// //     if(err) console.error(err);
+// //     else console.log("done");
+// // })
 
-fs.rename("hey.txt","hi.txt",function(err){
-    if(err) console.log(err);
-    else console.log("done")
-})
+// // fs.rename("hey.txt","hi.txt",function(err){
+// //     if(err) console.log(err);
+// //     else console.log("done")
+// // })
+
+// // fs.copyFile("hi.txt","./copy/copy.txt",function(err){
+// //     if(err) console.error(err);
+// //     else console.log("done");
+// // })
+
+// // fs.unlink("./copy/copy.txt",function(err){
+// //     if(err) console.error(err);
+// //     else
+// //         console.log("done");
+// // })
+
+// // fs.rm("./copy",{recursive:true},function(err){
+// //     if(err) console.error(err);
+// //     else console.log(err);
+// // })
+
+// const http = require('http');
+
+// const server=http.createServer(function(req,res){
+// res.end("hello world");
+// })
+
+// server.listen(3000);
